0063879a86b84bb220d11d98a2ef9419f3a3a8fd
gpealat <167517140+gpealat@users.noreply.github.com>
2024-04-23 18:15:52 +0200

    Improved code
    - Merged divisible and sound_generation functions in 1
    - used the looping on key on an array
    - used parameter expansion default value

--- Bash/Raindrops.sh
+++ Bash/Raindrops.sh
@@ -1,62 +1,33 @@
 #!/usr/bin/env bash
 
-# Create a function to check if the number is divisible by a certain value (the divisor)
-divisible(){
-
-    number=$1
-    divisor=$2
-
-    if [[ $(( "$number" % "$divisor" )) == 0 ]]
-    then
-        # If this is divisible, then return true
-        return 1
-    else
-        # If not return false
-        return 0
-    fi
-    
-}
-
 # Function to generate the sound depending on the divisibility
 sound_generation(){
     # The number is passed in argument 1
     # The divisor is passed in argument 2
-    divisible "$1" "$2"
-    divisible_by=$?
+    number=$1
+    divisor=$2
+    ! ((  "$number" % "$divisor"  == 0 ))
 
-    # If this is divisible, emit the sound which is passed in argument 3
-    if [[ "$divisible_by" == 1 ]]; then
-        return 1
-    fi
-
-    # If it is not divisible, then return nothing (empty answer)
-    return 0
 }
 
 
 # Creating an array to store the sounds
-sounds=("Pling" "Plang" "Plong")
+sounds=([3]='Pling' [5]='Plang' [7]='Plong')
 
 # Check the sound for different divisor
 i=0
-for divisor in 3 5 7; do
+for divisor in "${!sounds[@]}"; do
     sound_generation "$1" "$divisor"
     sound=$?
 
     # If the sound have to be generated
     if [[ sound -eq 1 ]]; then
-        factor+=${sounds[$i]}
+        factor+=${sounds[$divisor]}
+
     fi
     i="$i"+1
 done
 
-# Check if the result is empty (meaning it is not divisible by any number asked)
-if [[ -z "$factor" ]]
-then
-    # If this is empty, return the passed number
-    echo "$1"
-else
-    # If this is not empty, return the sound
-    echo "$factor"
-fi
+# Check if the result is empty (meaning it is not divisible by any number asked) - Using the default parameter in the Parameter expansion
+echo ${factor:-"$1"}
 
