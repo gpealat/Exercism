9b6207b80c605482cba24f7a712e120092eb6235
gpealat <167517140+gpealat@users.noreply.github.com>
2024-04-23 18:22:01 +0200

    Cleaned code after comments

--- Bash/reverse_string.sh
+++ Bash/reverse_string.sh
@@ -4,20 +4,13 @@
 length=${#1}
 string=$1
 
-# Testing for empty string
-if [[ "$length" == 0 ]];then
-    # Returning nothing if the first argument is empty
-    echo ""
-    exit
-fi
-
 # Creating the resulting string
 reversed=""
 
 # Looping in reverse
-for (( i=$(( "$length"-1 ));i>=0;i-- ));do
+for (( i= "$length"-1; i>=0; i-- ));do
     #Concatenating the string for the result
-    reversed="$reversed"${string:i:1}    
+    reversed+="${string:i:1}"    
 done
 
 echo "$reversed"
